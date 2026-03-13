58023e3c7a54f4dde3de6b59a62c04bde13b1ba7
saaviasole <swapnaliasole@yahoo.com>
2026-03-13 17:12:05 +0530

    space

--- main2.py
+++ main2.py
@@ -11,10 +11,10 @@ while p * p <= n:
         for i in range(p * p, n + 1, p):
             prime[i] = False
 
-        p += 1
+    p += 1
 
 print("Prime numbers are:")
 
 for i in range(2, n + 1):
     if prime[i]:
-        print(i, end="")
+        print(i,  end="")
